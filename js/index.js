f1634c02ddff206183f5b5c77cb9e44aca21de3f
Andreas200536996 <113151311+Andreas200536996@users.noreply.github.com>
2022-12-10 12:54:03 -0500

    additional changes

--- js/index.js
+++ js/index.js
@@ -1,30 +1,30 @@
 // Get the form element
 var form = document.getElementById('myForm');
 
-// Get the input elements where the user will enter their order
-var item1 = form.elements['item1'];
-var item2 = form.elements['item2'];
-var item3 = form.elements['item3'];
 
 // Add an event listener that will be triggered when the form is submitted
 form.addEventListener('submit', function(e) {
   // Prevent the form from being submitted
- // e.preventDefault();
+  e.preventDefault();
 
-  // Create a string to hold the items the user has ordered
-  var order = '';
 
-  // Check if each item is checked, and add its value to the order string if it is
-  if (item1.checked) {
-    order += item1.value + '\n';
-  }
-  if (item2.checked) {
-    order += item2.value + '\n';
-  }
-  if (item3.checked) {
-    order += item3.value + '\n';
-  }
 
-  // Create a JavaScript alert that displays the items the user has ordered
-  window.alert('You have ordered:\n' + order);
+
+
+  // Redirect the user to the thanks.html page
+  window.location.replace('thanks.html');
+
+// Get the form data from the previous page
+var formData = window.opener.formData;
+
+// Write the form data to the page
+document.write("Your Order: ")
+document.write('<dl>');
+for (var i in formData) {
+  document.write('<dt>' + i + '</dt>');
+  document.write('<dd>' + formData[i] + '</dd>');
+}
+document.write('</dl>');
+
 });
+
