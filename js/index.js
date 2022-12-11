8b785f8f61cddd789a501b068fbb42755ea94bbf
Andreas200536996 <113151311+Andreas200536996@users.noreply.github.com>
2022-12-10 23:31:11 -0500

    completed javascript to present total price

--- js/index.js
+++ js/index.js
@@ -1,30 +1,19 @@
-// Get the form element
-var form = document.getElementById('myForm');
+function submitForm() {
+    
+    var form = document.getElementById("myForm");
+  
+    
+    var breadInput = form.elements["breadNumber"];
+    var cakeInput = form.elements["cakeNumber"];
+    var muffinInput = form.elements["muffinNumber"]
+  
+    
+    var bread = breadInput.value;
+    var cake = cakeInput.value;
+    var muffin = muffinInput.value;
+    var totalPrice = ((bread*7.35) + (cake *17.85) + (muffin*1.47));
+  
+ alert("Your total order price will be: $" + totalPrice);
+  }
 
-
-// Add an event listener that will be triggered when the form is submitted
-form.addEventListener('submit', function(e) {
-  // Prevent the form from being submitted
-  e.preventDefault();
-
-
-
-
-
-  // Redirect the user to the thanks.html page
-  window.location.replace('thanks.html');
-
-// Get the form data from the previous page
-var formData = window.opener.formData;
-
-// Write the form data to the page
-document.write("Your Order: ")
-document.write('<dl>');
-for (var i in formData) {
-  document.write('<dt>' + i + '</dt>');
-  document.write('<dd>' + formData[i] + '</dd>');
-}
-document.write('</dl>');
-
-});
 
