7a21a5b77e442ed27c44bc3c0c63bc687a20d527
Andreas200536996 <113151311+Andreas200536996@users.noreply.github.com>
2022-12-11 16:11:02 -0500

    reformatted all pages

--- js/index.js
+++ js/index.js
@@ -1,19 +1,19 @@
 function submitForm() {
-    
+
     var form = document.getElementById("myForm");
-  
-    
+
+
     var breadInput = form.elements["breadNumber"];
     var cakeInput = form.elements["cakeNumber"];
     var muffinInput = form.elements["muffinNumber"]
-  
-    
+
+
     var bread = breadInput.value;
     var cake = cakeInput.value;
     var muffin = muffinInput.value;
-    var totalPrice = ((bread*7.35) + (cake *17.85) + (muffin*1.47));
-  
- alert("Your total order price will be: $" + totalPrice + "\nYour order contains: " + "\nBread: " + bread + "\nCake(s): " + cake +"\nMuffin(s): " + muffin);
-  }
+    var totalPrice = ((bread * 7.35) + (cake * 17.85) + (muffin * 1.47));
+
+    alert("Your total order price will be: $" + totalPrice + "\nYour order contains: " + "\nBread: " + bread + "\nCake(s): " + cake + "\nMuffin(s): " + muffin);
+}
 
 
